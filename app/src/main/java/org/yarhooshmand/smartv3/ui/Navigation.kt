6be00a6654b326c23482005441ef4adcfbb05fb8
ghadirb <ghadir.baraty@gmail.com>
2025-09-06 21:29:17 +0330

    Create Navigation.kt

---
+++ app/src/main/java/org/yarhooshmand/smartv3/ui/Navigation.kt
@@ -0,0 +1,23 @@
+package org.yarhooshmand.smartv3.ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "dashboard" // صفحه شروع اپ
+    ) {
+        composable("dashboard") {
+            DashboardScreen(navController)
+        }
+        composable("reminders") {
+            RemindersScreen(navController)
+        }
+    }
+}
