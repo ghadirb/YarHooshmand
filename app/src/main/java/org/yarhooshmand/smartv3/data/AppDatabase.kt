48d08bb971bb81fa7844d92cdf988dad14875da6
ghadirb <ghadir.baraty@gmail.com>
2025-09-06 13:27:38 +0330

    Update AppDatabase.kt

--- app/src/main/java/org/yarhooshmand/smartv3/data/AppDatabase.kt
+++ app/src/main/java/org/yarhooshmand/smartv3/data/AppDatabase.kt
@@ -1,30 +1,13 @@
 package org.yarhooshmand.smartv3.data
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Reminder::class], version = 2, exportSchema = false)
+@Database(
+    entities = [ReminderEntity::class],
+    version = 2, // نسخه جدید
+    exportSchema = true
+)
 abstract class AppDatabase : RoomDatabase() {
-
-    // ✅ فقط یک متد DAO
     abstract fun reminderDao(): ReminderDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: AppDatabase? = null
-
-        fun get(context: Context): AppDatabase =
-            INSTANCE ?: synchronized(this) {
-                INSTANCE ?: Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "reminder_db"
-                )
-                .fallbackToDestructiveMigration()
-                .build()
-                    .also { INSTANCE = it }
-            }
-    }
 }
