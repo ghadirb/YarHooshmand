117727cbae537a32db3a5d974f0560135d4cfa4a
ghadirb <ghadir.baraty@gmail.com>
2025-09-06 21:08:26 +0330

    Update MainActivity.kt

--- app/src/main/java/org/yarhooshmand/smartv3/MainActivity.kt
+++ app/src/main/java/org/yarhooshmand/smartv3/MainActivity.kt
@@ -3,13 +3,19 @@ package org.yarhooshmand.smartv3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import org.yarhooshmand.smartv3.ui.AppNav
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import org.yarhooshmand.smartv3.ui.Navigation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppNav()
+            MaterialTheme {
+                Surface {
+                    Navigation() // شروع از Navigation
+                }
+            }
         }
     }
 }
