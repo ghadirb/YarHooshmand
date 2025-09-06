37004c1d3afef2bd8b09fe080b5c4f786752225e
ghadirb <ghadir.baraty@gmail.com>
2025-09-06 19:25:59 +0330

    Delete app directory

--- app/src/main/java/org/yarhooshmand/smartv3/reminders/Toggle.kt
+++
@@ -1,9 +0,0 @@
-package org.yarhooshmand.smartv3.reminders
-
-import org.yarhooshmand.smartv3.data.ReminderEntity
-import org.yarhooshmand.smartv3.data.ReminderRepository
-
-suspend fun toggleDone(repo: ReminderRepository, reminder: ReminderEntity) {
-    val updated = reminder.copy(done = !reminder.done)
-    repo.update(updated)
-}
--- app/src/main/java/org/yarhooshmand/smartv3/widget/ReminderWidget.kt
+++
@@ -1,6 +0,0 @@
-
-package org.yarhooshmand.smartv3.widget
-
-// Placeholder for App Widget implementation.
-// Full AppWidgetProvider requires XML and AppWidget configuration which can be added later.
-object ReminderWidgetPlaceholder {}
